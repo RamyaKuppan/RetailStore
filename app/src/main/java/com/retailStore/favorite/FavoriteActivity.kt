5ea1566a64046ea0796c6a191dde5ee15f6497fe
RamyaTringapps <ramya.k@tringapps.com>
2018-07-03 06:21:56 -0700

    favorite feature

---
+++ app/src/main/java/com/retailStore/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.retailStore.favorite
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
---
+++ app/src/test/java/com/retailStore/productList/ProductListViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.retailStore.productList
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class ProductListViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+}
